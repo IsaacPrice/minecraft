0213fad4f453a965c1c6d4a396deb351b61d58bb
Isaac Price <100382457+IsaacPrice@users.noreply.github.com>
2024-05-02 19:21:08 -0400

    removed uneeded file

--- src/headers/generation.hpp
+++
@@ -1,6 +0,0 @@
-//#include "simplex_noise.hpp"
-
-// This will take a chunk and generate the blocks inside of it. It is a seperate file as the generation of blocks is a complex process.
-//void generateChunk(unsigned int seed) {
-//
-//}
